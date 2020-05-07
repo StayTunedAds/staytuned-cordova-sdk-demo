288dbb579ff151db482657d4bada40dc175c47b9
Antoine Leprevost <leprevost.antoine@gmail.com>
2020-05-07 14:58:01 +0200

    feat: add cordova app

---
+++ staytuned-cordova-sdk-demo/www/precache-manifest.0238b897fa925dbfbfb303fb57ab171a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a80149af997f822349343c4b6b0360c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f119397a254636d757e9",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c961e5470b32de34ddd",
+    "url": "./static/js/2.860bc2b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.860bc2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f119397a254636d757e9",
+    "url": "./static/js/main.c7601d03.chunk.js"
+  },
+  {
+    "revision": "f82064bef65ae25e9b7c",
+    "url": "./static/js/runtime-main.9da05cfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
